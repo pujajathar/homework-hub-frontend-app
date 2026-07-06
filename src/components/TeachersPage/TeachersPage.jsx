773493c879c789eff7379cf9d229680f724a275f
pujajathar <pujajathar91@.com>
2026-07-06 17:08:49 -0500

    Styled assignments section on teachers dashboard

--- src/components/TeachersPage/TeachersPage.jsx
+++ src/components/TeachersPage/TeachersPage.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import mockAssignments from "../mockAssignments";
 import AssignmentForm from "../AssignmentForm";
+import Footer from "../Footer/Footer";
 import './TeachersPage.css';
+
+
 
 
 function TeachersPage () {
     
    const [assignments, setAssignments] = useState(mockAssignments);
    const [showForm, setShowForm] = useState(false);
+ 
 
    const handleAddAssignment = (newAssignment) => {
      setAssignments((prev) => [
@@ -17,60 +21,91 @@ function TeachersPage () {
         {
          ...newAssignment,
             id:Date.now(),
+            completed:false
         },    
      ]);
 
     setShowForm(false);
 };
-    return (          
+    const toggleComplete = (id) => {
+        setAssignments((prev) => 
+        prev.map((a) => 
+        a.id === id ? {...a, completed : !a.completed} : a
+    )
+);
+    };
+    return (  
         
+        <div>   
+           
+    <div className="dashboard">
+        <div className="top-nav">
+        <Link to="/" className="home-link">🏠Home</Link>
+        </div>
 
-    <div>
-        <Link to={"/"}>Home</Link>
-         
-        <div>
-        
+        <div className="dashboard-header">
             <h1>Teacher Dashboard</h1>
             <p>Welcome back, Ms. Leah!</p>  
         </div>
-        <div className="assignment-section">
-            <div className="assignment-header">
+         <div className="main-grid">
+                <div className="card">
+                    <div className="card-header">
+
                 <h2>Assignments</h2> 
-           
-                <button onClick={() => setShowForm(true)}>
-                <span>+ Create New</span>
+
+                    <button
+                    className="btn-add" 
+                    onClick={() => setShowForm(true)}>
+                + Create New
                 </button>
-            </div>
-            {showForm && 
+                  </div>
+                  {showForm && (
             <AssignmentForm 
             onSubmit={handleAddAssignment}
             onCancel={() => setShowForm(false)}
             />
+            )
             }
-            <div>   
-                
-                      
+              
+           
                 <div className="assignment-list">
                       {/*display assignment*/ }
                {assignments.map((assignment) => (
-                <div key={assignment.id} className="assignment-card">
-                    <div className="arrange"> 
-                       <div className="title-duedate">
-                        <p>{assignment.title}</p>
-                        <p>{assignment.dueDate}</p>
-                     </div>
-                       <p><strong>{assignment.subject}</strong></p>
-                    
-                    </div>
-                </div>
-                   ))} 
-                
-                </div>         
-            </div>
+                <div key={assignment.id} className="assignment-row">
+                    <div className="assignment-left"> {/*left side */}
+                    <button 
+                    className="check-btn"
+                    onClick={() => toggleComplete(assignment.id)}>
+                        {assignment.completed ? "✅":"⬜"}
+                    </button>
 
-        </div>
+                      
+                        <div>
+                            <h3 className={assignment.completed ? "completed" : ''}>
+                                {assignment.title}
+                            </h3>
+                            <p className="due">
+                                Due {assignment.dueDate}
+                            </p>
+                            </div>
+                         </div>
+                         <div className="assignment-right">  {/*right side */}
+                            <span className={`tag ${assignment.subject.toLowerCase()}`}>
+                                {assignment.subject}
+                            </span>
+                         </div>
 
+                </div>  
+             ))}
+              </div> 
+              </div>
+          </div>
+          
     </div>
+    <Footer />
+   </div>   
+  
+    
 );  
 };
 export default TeachersPage;    
